46f2ea105411f5f5e8acb8202a03c7b482946515
Pawel Szczecina <231489@student.uek.krakow.pl>
2024-10-26 19:23:38 +0200

    Stwory

---
+++ Simulator/Creature.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection.Emit;
+using System.Xml.Linq;
+
+namespace Simulator;
+
+internal class Creature
+{
+    public string Name { get; set; }
+    public uint Level { get; set; }
+    public string Info { get { return $"{Name} [{Level}]"; } }
+    public Creature(string name, uint level=1)
+    {
+        Name = name;
+        Level = level;
+    }
+    public Creature() { }
+
+    public void SayHi()
+    {
+        Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
+    }
+}
--- Simulator/Program.cs
+++ Simulator/Program.cs
@@ -5,5 +5,11 @@ internal class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Starting Simulator!\n");
+        //testowanie czy "Animals" działa
+        //
+        //Animals a = new() { Description = "Dogs", Size = 5};
+        //Console.WriteLine(a.Info);
+        //
+        //wychodzi że działa
     }
 }
